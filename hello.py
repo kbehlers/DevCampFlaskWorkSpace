5cc736b8b08374ea0b0b285b2dea40c588209d37
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2018-11-16 15:59:54 -0700

    scraping is working

--- hello.py
+++ hello.py
@@ -4,6 +4,8 @@ import requests
 import indeed_request
 app = Flask(__name__)
 
+Data = []
+
 @app.route('/jobs', methods=["GET","POST"])
 def hello():
     if request.method == 'GET':
@@ -17,22 +19,35 @@ def hello():
         return jsonified_string
     if request.method == 'POST':
         # query = request.form.get('search')
-        # query=request.data.decode("utf-8", "replace")
+        query=request.data.decode("utf-8", "replace")
         query = request.data
         query = json.loads(query)
         query = query['search']
         response_list = indeed_request.retrieve_indeed(query)
+        # print(response_list)
         jsonified_string = jsonify(response_list)
-        jsonified_string.headers.add('Access-Control-Allow-Origin', '*')
+        # jsonified_string.headers.add('Access-Control-Allow-Origin', '*')
         # jsonified_string.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-        jsonified_string.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-        jsonified_string.headers["Content-Type"] = "application/json"
-        # return jsonified_string
-        return app.response_class(
-            response=json.dumps(response_list),
-            status=200,
-            mimetype='application/json'
-        )
+        # jsonified_string.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        # jsonified_string.headers["Content-Type"] = "application/json"
+        Data = response_list
+
+        print(Data)
+
+        return jsonified_string
+        # return app.response_class(
+        #     response=json.dumps(response_list),
+        #     status=200,
+        #     mimetype='application/json'
+        # )
+
+
+@app.route('/')
+def return_data():
+    print(Data)
+    jsonified_string = jsonify(Data)
+    return jsonified_string
+
 
 if __name__ == "__main__":
     app.run(debug=True)
