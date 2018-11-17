95b08e3eaded950cc43f1f3ac217e1e9e34e8828
kbehlers <kemais.ehlers@gmail.com>
2018-11-16 19:40:10 -0700

    state before refactor

--- hello.py
+++ hello.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 
 @app.route('/jobs', methods=["GET","POST"])
 def hello():
+    def returned_data(data):
+        return data
+    Data = returned_data("Hey")
     if request.method == 'GET':
         # response_dict = indeed_request.parse_indeed('output.txt')
         # jsonified_string = jsonify(response_dict)
@@ -15,10 +18,12 @@ def hello():
         jsonified_string = jsonify(response_list)
         return jsonified_string
     if request.method == 'POST':
-        query = request.form.get('query')
-        response_list = indeed_request.retrieve_indeed(query)
-        jsonified_string = jsonify(response_list)
-        return jsonified_string
+        
+        return "Hello"
+        # query = request.form.get('query')
+        # response_list = indeed_request.retrieve_indeed(query)
+        # jsonified_string = jsonify(response_list)
+        # return jsonified_string
 
 if __name__ == "__main__":
     app.run(debug=True)
